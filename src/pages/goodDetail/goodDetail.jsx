e5387286c9f17883e2897e0552c90795c9ec9ccc
congpeiran <congpeiran@tfrunning.com.cn>
2020-09-30 17:50:26 +0800

    商品详情~

--- src/pages/goodDetail/goodDetail.jsx
+++ src/pages/goodDetail/goodDetail.jsx
@@ -1,57 +1,67 @@
-
-import React, { Component } from 'react';
-import { Button, Toast, Carousel,Badge } from 'antd-mobile';
+import React, {Component} from 'react';
+import {Button, Toast, Carousel, Badge, Icon} from 'antd-mobile';
 import './goodDetail.scss';
 import axios from 'axios';
-import { inject ,observer } from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 
 @inject("carStore")
 @observer
 class shop extends Component {
-
     state = {
+        //接收入参
         id: '',
         proid: '',
+        //banner图
         banners: [],
-        outdata: [],
-        count:0,
-        shopCar:[]
+        bannerFlag:false,
+        //商品信息数据
+        infos: {},
+        count: 0,
+        shopCar: []
     }
 
     componentDidMount() {
         let id = this.props.match.params.id;
         let proid = this.props.match.params.proid;
-        this.setState({ id: id, proid: proid }, () => this.getshops())
+        this.setState({id: id, proid: proid}, () => this.getshops())
     }
 
+    //根据入参 获取商品详情
     getshops = () => {
         axios.get(`http://www.mei.com/appapi/product/detail/v3?categoryId=${this.state.id}&productId=${this.state.proid}&platform_code=H5&timestamp=1601174880152&summary=2dd5d65d596a88cb0bf8e522d69e76aa`).then(
             res => {
+                console.log('res.data', res.data)
                 this.setState({
-                    outdata: res.data.infos,
-                    banners: res.data.infos.images
+                    infos: res.data.infos,
+                    banners: res.data.infos.images,
+                    bannerFlag:true
                 })
             }
         )
     }
 
-    goback = () => {
-        window.history.back(-1)
+    //回到上一页
+    goBack = () => {
+        window.history.back(-1);
     }
+
+    //右侧按钮
     tip = () => {
         Toast.offline('这里还没进行开发呢！', 1.5);
     }
 
-    joincart=()=>{
+    joincart = () => {
         let carData = this.state.shopCar
-        carData.push(this.state.outdata)
-        this.setState({shopCar:carData},()=>{this.setData()})
+        carData.push(this.state.infos)
+        this.setState({shopCar: carData}, () => {
+            this.setData()
+        })
     }
 
-    setData=()=>{
-        if(this.props.carStore.count === 21){
-            Toast.info('最多添加21件商品哦~快去结算吧！',2)
-        }else{
+    setData = () => {
+        if (this.props.carStore.count === 21) {
+            Toast.info('最多添加21件商品哦~快去结算吧！', 2)
+        } else {
             this.props.carStore.setData(this.state.shopCar)
         }
     }
@@ -60,53 +70,95 @@ class shop extends Component {
     render() {
         return (
             <div className="goodbox">
-                <div className="oneBox">
-                    <Button onClick={this.goback} size="small" icon="left" style={{ marginLeft: 10 }}></Button>
-                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>{this.state.outdata.brand}</p>
-                    <Button onClick={this.tip} size="small" style={{ marginRight: 10 }}>...</Button>
+                <div className="headerBox">
+                    <div onClick={() => {
+                        this.goBack()
+                    }} className="btnBoxL">
+                        <Icon type="left"/>
+                    </div>
+                    <div className="centerTextBox">
+                        <div className="centerText1">{this.state.infos.brand}</div>
+                        <div className="centerText2">￥{this.state.infos.price}</div>
+                    </div>
+                    <div onClick={() => {
+                        this.tip()
+                    }} className="btnBoxR">
+                        <Icon type="ellipsis"/>
+                    </div>
                 </div>
-                <Carousel
-                    autoplay
-                    infinite
-                >
-                    {this.state.banners.map(val => (
-                        <a
-                            key={val}
-                            href="/"
-                            style={{ display: 'block', width: '100%' }}
-                        >
-                            <img
-                                src={val.smallImgUrl}
-                                alt=""
-                                style={{ width: '90%', verticalAlign: 'top', marginLeft: '5%' }}
-                                onLoad={() => {
-                                    // fire window resize event to change height
-                                    window.dispatchEvent(new Event('resize'));
-                                    this.setState({ imgHeight: 'auto' });
-                                }}
-                            />
-                        </a>
-                    ))}
-                </Carousel>
-                <p className="name">{this.state.outdata.name}</p>
-                <div className="pribox">
-                    <p className="price">¥{this.state.outdata.price}</p>
-                    <p className="dis">&nbsp;&nbsp;{this.state.outdata.discount}</p>
-                    <p style={{ textDecoration: 'line-through', color: '#ccc',marginLeft:30 }}> {this.state.outdata.marketPrice}</p>
-                    {/* <i className="icon-cart joincar"></i> */}
+                <div className="contentBox">
+                    <div className="bannerImgBox">
+                        {this.state.bannerFlag ?<Carousel autoplay={true} infinite={true}>
+                            {this.state.banners.map((val, index) => (
+                                <img src={val.smallImgUrl}
+                                     alt=""
+                                     key={index}
+                                     className="bannerImg"
+                                     onLoad={() => {
+                                         window.dispatchEvent(new Event('resize'));
+                                         this.setState({imgHeight: 'auto'});
+                                     }}
+                                />
+                            ))}
+                        </Carousel> : null}
+                    </div>
+                    <div className="product-name">
+                        <div className="nameText">{this.state.infos.name}</div>
+                    </div>
+                    <div className="priceBox1">
+                        <div className="priceBox1-text">￥{this.state.infos.marketPrice}</div>
+                    </div>
+                    <div className="priceBox2">
+                        <div className="priceBox2-text">¥{this.state.infos.price}</div>
+                        <div className="priceBox2-right">{this.state.infos.discount}</div>
+                    </div>
+                    <div className="deliveryTime">
+                        <img alt="" className="deliveryTime-img" src={require("../../assets/img/time.png")}/>
+                        <div className="deliveryTime-text">{this.state.infos.deliver_date}</div>
+                    </div>
+                    <div className="listItem">
+                        <div className="listItem-left">闪购</div>
+                        <div className="listItem-right"></div>
+                    </div>
+                    <div className="listItem">
+                        <div className="listItem-left">服务</div>
+                        <div className="listItem-right"></div>
+                    </div>
+                    <div className="listItem">
+                        <div className="listItem-left">尺码</div>
+                        <div className="listItem-right"></div>
+                    </div>
+                    <div className="detailsTitle">
+                        <div className="detailsTitle-text">商品参数</div>
+                    </div>
+                    <div className="detailsTitle">
+                        <div className="detailsTitle-text">商品详情</div>
+                    </div>
                 </div>
+
+
+                {/*<div className="pribox">*/}
+                {/*    <p className="price">¥{this.state.infos.price}</p>*/}
+                {/*    <p className="dis">&nbsp;&nbsp;{this.state.infos.discount}</p>*/}
+                {/*    <p style={{*/}
+                {/*        textDecoration: 'line-through',*/}
+                {/*        color: '#ccc',*/}
+                {/*        marginLeft: 30*/}
+                {/*    }}> {this.state.infos.marketPrice}</p>*/}
+                {/*     <i className="icon-cart joincar"></i> */}
+                {/*</div>*/}
                 <div className="carbox">
-                <div className="bag">
-                    <i className="icon-cart cart"></i>
-                    <Badge text={this.props.carStore.count} overflowCount={20} />
+                    <div className="bag">
+                        <i className="icon-cart cart"></i>
+                        <Badge text={this.props.carStore.count} overflowCount={20}/>
+                    </div>
+                    <div className="join" onClick={this.joincart}>
+                        <p className="joinp">加入购物车</p>
+                    </div>
+                    <div className="buy">
+                        <p className="buyp">立即购买</p>
+                    </div>
                 </div>
-                <div className="join" onClick={this.joincart}>
-                    <p className="joinp">加入购物车</p>
-                </div>
-                <div className="buy">
-                    <p  className="buyp">立即购买</p>
-                </div>
-            </div>
             </div>
         )
     }
